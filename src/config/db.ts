5820348f02081461bc1f87b582b05edd1562afd3
Sneara0 <snearaparvin.cse@gmail.com>
2025-12-08 02:20:53 +0600

    add file

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,15 +1,17 @@
-import {Pool} from 'pg'
+import { Pool } from "pg";
 import config from ".";
 
-export const pool=new Pool({
-    connectionString: `${config.connection_str}`
-})
+
+export const pool = new Pool({
+  connectionString: config.connection_str,
+  ssl: { rejectUnauthorized: false }
+});
+
 
 export const initDB = async () => {
   console.log("Initializing DB...");
 
   try {
-  
     await pool.query(`
       CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
@@ -24,7 +26,6 @@ export const initDB = async () => {
       );
     `);
 
-  
     await pool.query(`
       CREATE TABLE IF NOT EXISTS vehicles (
         id SERIAL PRIMARY KEY,
@@ -38,25 +39,24 @@ export const initDB = async () => {
       );
     `);
 
-   
-   
- await pool.query(`
+    await pool.query(`
       CREATE TABLE IF NOT EXISTS bookings (
-  id SERIAL PRIMARY KEY,
-  customer_id INT REFERENCES users(id) ON DELETE CASCADE,
-  vehicle_id INT REFERENCES vehicles(id) ON DELETE CASCADE,
-  rent_start_date DATE NOT NULL,
-  rent_end_date DATE NOT NULL,
-  total_price NUMERIC(10,2),
-  status VARCHAR(20) DEFAULT 'active',
-  created_at TIMESTAMP DEFAULT NOW(),
-  updated_at TIMESTAMP DEFAULT NOW()
+        id SERIAL PRIMARY KEY,
+        customer_id INT REFERENCES users(id) ON DELETE CASCADE,
+        vehicle_id INT REFERENCES vehicles(id) ON DELETE CASCADE,
+        rent_start_date DATE NOT NULL,
+        rent_end_date DATE NOT NULL,
+        total_price NUMERIC(10,2),
+        status VARCHAR(20) DEFAULT 'active',
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
       );
     `);
-    console.log("Database initialized successfully!");
+
+    console.log("✅ Database initialized successfully!");
   } catch (error) {
-    console.error("Error initializing database:", error);
+    console.error("❌ Error initializing database:", error);
   }
 };
 
-export default initDB
+export default initDB;
